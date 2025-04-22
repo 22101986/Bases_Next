6a8468dc544256ffff3dc238e63d89f39ffa2fff
hpierrac <h.pierrache@gmail.com>
2025-04-22 16:23:23 +0200

    create project

---
+++ app/about.js
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+
+export default function About() {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>À propos</h1>
+      <p>Ceci est notre page À propos</p>
+    </main>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>À propos</h1>
+      <p>Ceci est notre page À propos</p>
+    </main>
+  )
+}
